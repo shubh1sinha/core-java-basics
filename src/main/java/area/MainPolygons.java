81fbfcd1077a945cb5efde76c987dc3454bce5fe
shubh1sinha <sinhashubh04@gmail.com>
2022-10-18 22:28:11 +0530

    basics area qurstion committed

---
+++ src/main/java/area/MainPolygons.java
@@ -0,0 +1,15 @@
+package com.area.java;
+
+public class MainPolygons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Polygons t = new Triangle(2, 3);
+		t.area();
+
+		Polygons rect = new Rectangle(5, 6);
+		rect.area();
+
+	}
+
+}
---
+++ src/main/java/area/Polygons.java
@@ -0,0 +1,18 @@
+package com.area.java;
+
+public abstract class Polygons {
+	protected int dim1;
+	protected int dim2;
+
+	public Polygons(int dim1, int dim2) {
+		this.dim1 = dim1;
+		this.dim2 = dim2;
+	}
+
+	public Polygons(int dim2) {
+		this.dim2 = dim2;
+	}
+
+	public abstract void area();
+
+}
